9f9115e2285b007ba07f58e1a4e627814224b19d
DarthVictor <follet.victor@gmail.com>
2013-06-17 16:45:56 +0400

    Create  MessageRenderer.java

---
+++ ch4/src/main/java/com/apress/prospring3/ch4/ MessageRenderer.java
@@ -0,0 +1,16 @@
+package com.apress.prospring3.ch4;
+
+/**
+ * @author Clarence
+ *
+ */
+public interface MessageRenderer 
+{
+
+    public void render();
+
+	public void setMessageProvider(MessageProvider provider);
+
+	public MessageProvider getMessageProvider();
+
+}
