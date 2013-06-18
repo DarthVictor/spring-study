6c11ba77b2e2c64f29b79cfc0ca0172bed53d5b5
Follet Victor <follet.victor@gmail.com>
2013-06-19 02:16:46 +0400

    Declare Spring Components

---
+++ ch4/src/main/java/com/apress/prospring3/ch4/xml/HelloWorldMessageProvider.java
@@ -0,0 +1,12 @@
+package com.apress.prospring3.ch4.xml;
+
+import com.apress.prospring3.ch4.MessageProvider;
+
+public class HelloWorldMessageProvider implements MessageProvider {
+
+	@Override
+	public String getMessage() {
+		return "Hello World!";
+	}
+
+}
