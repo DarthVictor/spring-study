65a5e9bd4a1a69bd82a581e041c123713413950f
Follet Victor <follet.victor@gmail.com>
2013-06-19 02:30:33 +0400

    Listing 4-20. Declare Spring Beans (Testing)

---
+++ ch4/src/main/java/com/apress/prospring3/ch4/DeclareSpringComponents.java
@@ -0,0 +1,15 @@
+package com.apress.prospring3.ch4;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+public class DeclareSpringComponents 
+{
+	public static void main(String[] args) 
+	{
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:app-context-annotation.xml");
+		
+		MessageProvider messageProvider = ctx.getBean("messageProvider",
+				MessageProvider.class);
+		System.out.println(messageProvider.getMessage());
+	}
+}
