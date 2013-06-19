bd7c34a34fa022881e0f51c08796d36315575214
Follet Victor <follet.victor@gmail.com>
2013-06-20 01:00:47 +0400

    Constructor Confusion

---
+++ ch4/src/main/java/com/apress/prospring3/ch4/xml/ConstructorConfusion.java
@@ -0,0 +1,37 @@
+package com.apress.prospring3.ch4.xml;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class ConstructorConfusion 
+{
+	private String someValue;
+	
+	
+	
+	@Override
+	public String toString() {
+		return someValue;
+	}
+
+	public ConstructorConfusion(String someValue) {
+		System.out.println("ConstructorConfusion(String) called");
+		this.someValue = someValue;
+	}
+
+	public ConstructorConfusion(int someValue) {
+		System.out.println("ConstructorConfusion(int) called");
+		this.someValue = String.valueOf(someValue);
+	}
+	
+	public static void main(String[] args) 
+	{
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:app-context-xml.xml");
+		ctx.refresh();
+		
+		ConstructorConfusion cc = (ConstructorConfusion)ctx.getBean(
+								"constructorConfusion");		
+		System.out.println(cc);
+	}	
+	
+}
