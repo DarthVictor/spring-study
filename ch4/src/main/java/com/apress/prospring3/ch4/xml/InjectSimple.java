78c6184b4f75a1964be61b650eac549e3992b8ef
DarthVictor <follet.victor@gmail.com>
2013-06-24 13:30:52 +0400

    Update InjectSimple.java

--- ch4/src/main/java/com/apress/prospring3/ch4/xml/InjectSimple.java
+++ ch4/src/main/java/com/apress/prospring3/ch4/xml/InjectSimple.java
@@ -2,6 +2,22 @@ package com.apress.prospring3.ch4.xml;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class InjectSimple {
+public class InjectSimple 
+{
+
+    private String name;
+    private int age;
+    private float height;
+    private boolean programmer;
+    private Long ageInSeconds;
+
+    public static void main(String[] args)
+    {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:app-context-xml.xml");
+        ctx.refresh();
+        
+        InjectSimple simple
+    }
 
 }
